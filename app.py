61e85907de1b2a377fd01dae5f4cb1d3073c4d5d
Samiris <samirisgomesdeoliveira7@gmail.com>
2026-03-17 11:27:09 -0300

    Update app.py

--- app.py
+++ app.py
@@ -1,11 +1,8 @@
-# Solicita as 4 notas do usuário
-nota1 = float(input("Digite a primeira nota: "))
-nota2 = float(input("Digite a segunda nota: "))
-nota3 = float(input("Digite a terceira nota: "))
-nota4 = float(input("Digite a quarta nota: "))
+n1 = float(input("Digite a primeira nota: "))
+n2 = float(input("Digite a segunda nota: "))
+n3 = float(input("Digite a terceira nota: "))
+n4 = float(input("Digite a quarta nota: "))
 
-# Calcula a média
-media = (nota1 + nota2 + nota3 + nota4) / 4
+media = (n1 + n2 + n3 + n4) / 4
 
-# Exibe o resultado com duas casas decimais
 print(f"A média do aluno é: {media:.2f}")
